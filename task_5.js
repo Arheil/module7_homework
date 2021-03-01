d682015a1c3c54ec80bd2992cfbeb346a3a8da69
Stanislav <verman19837@gmail.com>
2021-03-01 21:49:43 +0700

    add task_5

--- task_5.js
+++ task_5.js
@@ -1,2 +1,80 @@
 // Переписать консольное приложение из предыдущего юнита на классы.
 
+class Electrick {
+  constructor(voltage, enable, secInHour){
+  this.voltage = voltage;
+  this.enable = enable;
+  this.secInHour = secInHour;
+  }
+    turnOn(voltage){
+      this.enable = true;
+      this.voltage = voltage;
+  }
+    turnOff(voltage){
+    this.enable = false;
+    voltage = 0;
+  }
+   getUsePower(secInHour){
+    if(this.enable === false){
+      return 0;
+    }else{
+       let kW = this.voltage * secInHour;
+       return kW;
+    }
+   
+  } 
+}
+
+class TV extends Electrick {
+  constructor(voltage, enable, secInHour){
+    super(voltage, enable, secInHour);
+  }
+}
+
+let tv = new TV(30,true,760);
+
+console.log(tv.getUsePower(760));
+
+class Lamp extends Electrick {
+  constructor(voltage, enable, secInHour){
+    super(voltage, enable, secInHour);
+  }
+}
+
+let lamp = new Lamp(20,true,1000);
+
+class ElectrickStove extends Electrick {
+  constructor(voltage, enable, secInHour, hotplate){
+    super(voltage, enable, secInHour);
+  }
+  
+  turnOnHotplate(hotplate){
+  return this.voltage * hotplate;
+}
+  
+  getUsePower(secInHour, hotplate){
+  if(!hotplate){
+    return this.voltage * secInHour;
+  }else{
+    let countHotplate = this.turnOnHotplate(hotplate);
+    return this.voltage * secInHour + countHotplate;
+  }
+}
+}
+
+let electrickStove = new ElectrickStove(100,false,760);
+
+
+// console.log(electrickStove.turnOnHotplate(3));
+
+// console.log(electrickStove.getUsePower(300,2))
+
+// console.log(electrickStove.turnOnHotplate(3,''))
+
+// console.log(tv.getUsePower(580));
+
+// console.log(tv.getUsePower(580) + lamp.getUsePower(760));
+
+// tv.turnOff()
+
+// console.log(tv.getUsePower(50,360));
